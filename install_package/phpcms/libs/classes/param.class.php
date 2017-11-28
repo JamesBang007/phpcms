611d3fa1e2c1d2760007e0ea959f34fb50bea046
snow <phpcms@msn.com>
2017-11-28 09:40:33 +0800

    修复文件包含漏洞

--- install_package/phpcms/libs/classes/param.class.php
+++ install_package/phpcms/libs/classes/param.class.php
@@ -105,14 +105,15 @@ class param {
 	 * @return mixed 成功则返回cookie 值，否则返回 false
 	 */
 	public static function get_cookie($var, $default = '') {
-		$var = pc_base::load_config('system','cookie_pre').$var;
-		$value = isset($_COOKIE[$var]) ? sys_auth($_COOKIE[$var], 'DECODE', md5(PC_PATH.'cookie'.$var).pc_base::load_config('system','auth_key')) : $default;
-		if(in_array($var,array('_userid','userid','siteid','_groupid','_roleid'))) {
-			$value = intval($value);
-		} elseif(in_array($var,array('_username','username','_nickname','admin_username','sys_lang'))) { //  site_model auth
-			$value = safe_replace($value);
-		}
-		return $value;
+		$var_base = $var;
+                $var = pc_base::load_config('system','cookie_pre').$var;
+                $value = isset($_COOKIE[$var]) ? sys_auth($_COOKIE[$var], 'DECODE', md5(PC_PATH.'cookie'.$var).pc_base::load_config('system','auth_key')) : $default;
+                if(in_array($var_base,array('_userid','userid','siteid','_groupid','_roleid'))) {
+                        $value = intval($value);
+                } elseif(in_array($var_base,array('_username','username','_nickname','admin_username','sys_lang'))) { //  site_model auth
+                        $value = safe_replace($value);
+                }
+                return $value;
 	}
 
 	/**
